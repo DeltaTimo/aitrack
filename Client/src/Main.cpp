4f6879244fdc33df022dad05a63eb02958eb6b2e
Timo Zuccarello <timo@zuccarello.eu>
2024-10-17 16:39:24 +0200

    Use different functions for setting envars on Windows and Linux.

--- Client/src/Main.cpp
+++ Client/src/Main.cpp
@@ -12,7 +12,7 @@
 
 #include "model/UpdateChecker.h"
 
-
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
@@ -23,10 +23,19 @@ int main(int argc, char *argv[])
 
     auto conf_mgr = std::make_unique<ConfigMgr>("./prefs.ini");
 
+#ifdef __linux
+    char num_threads_env[] = "OMP_NUM_THREADS=XXXXXX";
+    std::memset(&num_threads_env, 0, sizeof(num_threads_env));
+#endif
     auto state = conf_mgr->getConfig();
     if (state.onnx_set_env_threads) {
+#ifdef _WIN32
         std::wstring ws = std::to_wstring(state.onnx_env_threads);
         SetEnvironmentVariable(LPWSTR(L"OMP_NUM_THREADS"), ws.c_str());
+#else
+        sprintf(num_threads_env, "OMP_NUM_THREADS=%d", state.onnx_env_threads);
+        putenv(num_threads_env);
+#endif
     }
     if (state.onnx_set_num_threads) {
         omp_set_num_threads(state.onnx_num_threads);
